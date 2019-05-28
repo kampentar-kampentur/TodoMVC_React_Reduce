9860eea8097ba48a40e20cd3058379068c7d6134
koksss03 <koksss03@gmail.com>
2019-05-28 17:04:19 +0300

    SavaStorage to subscribe

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import { createStore } from 'redux';
 import * as serviceWorker from './serviceWorker';
 import reducer from './reducers';
 import Home from './views/Home';
+import localStorage from './assets/js/saveState'
 
 const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+store.subscribe(() => localStorage.saveState('redux', store.getState().todoList))
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/todoList.js
+++ src/reducers/todoList.js
@@ -5,38 +5,30 @@ export default (state = localStorage.restoreState(storageId), action) => {
   let todoList, index;
   switch (action.type) {
     case 'ADD_TODO':
-      todoList = [...state, action.todo]
-      localStorage.saveState(storageId, todoList);
-      return todoList
+      return [...state, action.todo]
     case 'CHANGE_TODO_STATE':
-      todoList = state.slice();
-      index = todoList.findIndex(todo => todo.id === action.id);
-      if (index === -1) return todoList;
-      todoList[index].checked = !todoList[index].checked;
-      localStorage.saveState(storageId, todoList);
-      return todoList;
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          todo.checked = !todo.checked;
+        }
+        return todo
+      });
     case 'REMOVE_TODO':
-      todoList = state.slice();
-      index = todoList.findIndex(todo => todo.id === action.id);
-      if (index === -1) return todoList;
-      todoList.splice(index, 1);
-      localStorage.saveState(storageId, todoList);
-      return todoList;
+      return state.filter(todo => todo.id !== action.id);
     case 'CHANGE_STATE_ALL_TODO':
-      todoList = state.slice();
-      todoList.forEach(todo => todo.checked = action.toggleAllState);
-      localStorage.saveState(storageId, todoList);
-      return todoList;
+      return state.map(todo => {
+        todo.checked = action.toggleAllState
+        return todo
+      });
     case 'CLEAR_COMPLETED':
-      todoList = state.filter(todo => !todo.checked);
-      localStorage.saveState(storageId, todoList);
-      return todoList;
+      return state.filter(todo => !todo.checked);
     case 'EDIT_TODO':
-      todoList = state.slice();
-      index = todoList.findIndex(todo => todo.id === action.id);
-      todoList[index].value = action.value;
-      localStorage.saveState(storageId, todoList);
-      return todoList;
+      return state.map(todo => {
+        if (todo.id === action.id) {
+          todo.checked = !todo.checked;
+        }
+        return todo
+      });
     default:
       return state;
   }
